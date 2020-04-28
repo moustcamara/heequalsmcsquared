224c96ecca562747737c5f09b44722f4febbd5ae
themefiend <moust@moustcamara.com>
2020-04-28 10:09:41 -0400

    Applied tracklist styling changes to correct component.

--- components/TracksCardAlt.js
+++ components/TracksCardAlt.js
@@ -17,10 +17,12 @@ let TracksCardAlt = props => {
                     background-image: url(https://www.transparenttextures.com/patterns/asfalt-light.png);
                 }
 
-                .tracklist-card__inner {
-                    width: 90%;
-                    margin-top: 50px
+               .tracklist-card__inner {
+                    width: 80%;
+                    margin-top: 75px;
+                    margin-bottom: 75px
                 }
+
             `}</style>
 
             <iframe width="100%" height="800" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1038660490&color=%23040404&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
